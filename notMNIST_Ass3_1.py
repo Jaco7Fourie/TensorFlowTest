d2f80ddd827730a41d469b193320dc0f6ee2ecf8
jacofourie@gmail.com <KG4ag82crtGsIR>
2017-01-30 17:31:13 +1300

    Started assignment 3: Regularisation

---
+++ notMNIST_Ass3_1.py
@@ -0,0 +1,82 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+
+pickle_file = 'notMNIST.pickle'
+
+with open(pickle_file, 'rb') as f:
+  save = pickle.load(f)
+  train_dataset = save['train_dataset']
+  train_labels = save['train_labels']
+  valid_dataset = save['valid_dataset']
+  valid_labels = save['valid_labels']
+  test_dataset = save['test_dataset']
+  test_labels = save['test_labels']
+  del save  # hint to help gc free up memory
+  print('Training set', train_dataset.shape, train_labels.shape)
+  print('Validation set', valid_dataset.shape, valid_labels.shape)
+  print('Test set', test_dataset.shape, test_labels.shape)
+
+image_size = 28
+num_labels = 10
+
+
+def reformat(dataset, labels):
+    dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+    # Map 1 to [0.0, 1.0, 0.0 ...], 2 to [0.0, 0.0, 1.0 ...]
+    labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+
+# use tensorflow to build multinomial logistic regression using stochastic gradient descent.
+batch_size = 128
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
+
+graph = tf.Graph()
+with graph.as_default():
+    # Input data. For the training data, we use a placeholder that will be fed
+    # at run time with a training minibatch.
+    tf_train_dataset = tf.placeholder(tf.float32,
+                                      shape=(batch_size, image_size * image_size))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
+
+    # Variables.
+    L1_weights = tf.Variable(tf.truncated_normal([image_size * image_size, hidden_nodes]))
+    L1_biases = tf.Variable(tf.zeros([hidden_nodes]))
+    L2_weights = tf.Variable(tf.truncated_normal([hidden_nodes, num_labels]))
+    L2_biases = tf.Variable(tf.zeros([num_labels]))
+
+    # one pass forward from images to logits
+    def propForward(input):
+        L1 = tf.matmul(input, L1_weights) + L1_biases
+        L2_inputs = tf.nn.relu(L1)
+        return tf.matmul(L2_inputs, L2_weights) + L2_biases
+
+
+    # Training computation.
+    logits = propForward(tf_train_dataset)
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+
+    # Optimizer.
+    optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+    # Predictions for the training, validation, and test data.
+    train_prediction = tf.nn.softmax(logits)
+    valid_prediction = tf.nn.softmax(propForward(tf_valid_dataset))
+    test_prediction = tf.nn.softmax(propForward(tf_test_dataset))
+
+num_steps = 3001
